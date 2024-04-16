68b26a0cfaf88e277314c9973ebd13b5736f69ac
ram <rramkumar1306@gmail.com>
2024-04-16 22:34:14 +0530

    Refactored.

--- src/store/reducers/todo.js
+++ src/store/reducers/todo.js
@@ -1,20 +1,24 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
+const initialState = { todos: [], todo: "" };
 
 const todoSlice = createSlice({
-    name: "todo",
-    initialState: { todos: [] },
-    reducers: {
-      addTodo: (state, actions) => {
-        state.todos.push(actions.payload);
-      },
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, actions) => {
+      state.todos.push(actions.payload);
     },
-    prepare: (text) => {
-      var id = nanoid();
-      return { payload: { id, text } };
-    },
-  });
+    typeTodo:(state, actions)=>{
+      state.todo = actions.payload
+    }
+  },
+  prepare: (text) => {
+    var id = nanoid();
+    return { payload: { id, text } };
+  },
+});
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, typeTodo } = todoSlice.actions;
 
-export default todoSlice.reducer
+export default todoSlice.reducer;
