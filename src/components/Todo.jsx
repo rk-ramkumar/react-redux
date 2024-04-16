49c8bfa93d0d703a872846b26ae2173586b341a7
ram <rramkumar1306@gmail.com>
2024-04-16 22:50:35 +0530

    Implemented addTodo logic.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { typeTodo } from "../store/reducers/todo";
+import { addTodo, setNewTask } from "../store/reducers/todo";
 
 const Todo = () => {
   const state = useSelector((state) => state.todo);
   const dispatch = useDispatch();
 
-  function handleAddTodo() {}
+  function handleAddTodo() {
+    dispatch(addTodo(state.newTask));
+  }
 
   return (
     <div>
       <input
         placeholder="Enter your task..."
-        onChange={({ target: { value } }) => dispatch(typeTodo(value))}
+        value={state.newTask}
+        onChange={({ target: { value } }) => dispatch(setNewTask(value))}
       />
       <button onClick={handleAddTodo}>Add</button>
 
       <div>
         {state.todos.map((todo, idx) => (
-          <div key={idx}> todo </div>
+          <div key={idx}> {todo.task} </div>
         ))}
       </div>
     </div>
--- src/store/reducers/todo.js
+++ src/store/reducers/todo.js
@@ -1,24 +1,28 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-const initialState = { todos: [], todo: "" };
+const initialState = { todos: [], newTask: "" };
 
 const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
-    addTodo: (state, actions) => {
-      state.todos.push(actions.payload);
+    addTodo: {
+      reducer: (state, actions) => {
+        state.todos.push(actions.payload);
+        state.newTask = ""
+      },
+      prepare: (task) => {
+        var id = nanoid();
+        return { payload: { id, task } };
+      },
     },
-    typeTodo:(state, actions)=>{
-      state.todo = actions.payload
-    }
-  },
-  prepare: (text) => {
-    var id = nanoid();
-    return { payload: { id, text } };
+
+    setNewTask: (state, actions) => {
+      state.newTask = actions.payload;
+    },
   },
 });
 
-export const { addTodo, typeTodo } = todoSlice.actions;
+export const { addTodo, setNewTask } = todoSlice.actions;
 
 export default todoSlice.reducer;
