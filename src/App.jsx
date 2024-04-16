8abdb840a350c28f5b0d09017185691d2a120c5d
ram <rramkumar1306@gmail.com>
2024-04-16 22:21:39 +0530

    Refactored.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import  { increament } from "./store";
 import { useDispatch, useSelector } from "react-redux";
+import { increament } from "./store/reducers/counter";
 
 function App() {
   const count = useSelector(state=> state.counter.value);
   const dispatch = useDispatch();
-  const todo = useSelector(console.log)
+
   return (
     <>
       <div className="card">
