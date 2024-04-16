373502bd323e0589632faffe4e47e8ddeccfa00b
ram <rramkumar1306@gmail.com>
2024-04-16 09:22:01 +0530

    Bug fixed.

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,24 @@
-const { createSlice, configureStore } = require("@reduxjs/toolkit");
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const couterSlice = createSlice({
-    name: 'counter',
-    initialState: {
-        value: 0
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increament: (state) => {
+      state.value += 1;
     },
-    reducers:{
-        increament: state=>{
-            state.value += 1
-        },
-        decrement: state =>{
-            state -= 1
-        }
-    }
-})
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
 
-const {increament, decrement} = couterSlice.actions
+export const { increament, decrement } = couterSlice.actions;
 
 const store = configureStore({
-    reducer: couterSlice
-})
+  reducer: couterSlice.reducer,
+});
+
+export default store;
