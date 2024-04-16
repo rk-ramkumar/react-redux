12aae238cb384d0c21618c19508cb801319270da
ram <rramkumar1306@gmail.com>
2024-04-16 22:34:30 +0530

    Added todo to main.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increament } from "./store/reducers/counter";
+import Todo from "./components/Todo";
 
 function App() {
   const count = useSelector(state=> state.counter.value);
@@ -12,6 +13,7 @@ function App() {
         <button onClick={() => dispatch(increament())}>
           count is {count}
         </button>
+        <Todo/>
       </div>
     </>
   );
