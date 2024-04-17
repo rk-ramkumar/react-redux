1429be627f2186998d0cc1a778d12e7445786f29
ram <rramkumar1306@gmail.com>
2024-04-17 08:42:28 +0530

    Introduced Asyncthunk.

--- src/store/reducers/todo.js
+++ src/store/reducers/todo.js
@@ -1,6 +1,12 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, nanoid } from "@reduxjs/toolkit";
 
-const initialState = { todos: [], newTask: "" };
+const initialState = { todos: [], newTask: "", isLoading: false };
+
+export const fetchTodo = createAsyncThunk("todo/fetchTodos",async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+      return res.json
+    })
 
 const todoSlice = createSlice({
   name: "todo",
@@ -9,7 +15,7 @@ const todoSlice = createSlice({
     addTodo: {
       reducer: (state, actions) => {
         state.todos.push(actions.payload);
-        state.newTask = ""
+        state.newTask = "";
       },
       prepare: (task) => {
         var id = nanoid();
@@ -21,6 +27,12 @@ const todoSlice = createSlice({
       state.newTask = actions.payload;
     },
   },
+
+  extraReducers: {
+    [fetchTodo.pending]:(state)=>{
+      state.isLoading = true
+    }
+  }
 });
 
 export const { addTodo, setNewTask } = todoSlice.actions;
